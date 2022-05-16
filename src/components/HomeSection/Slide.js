54aa664d7b7afd0de5f5166f3b668f247f1d4a3e
sasindusensly <sasindusubodhaka@gmail.com>
2022-05-17 00:53:13 +0530

    slide form

--- src/components/HomeSection/Slide.js
+++ src/components/HomeSection/Slide.js
@@ -37,6 +37,26 @@ const Slide = ({ slides }) => {
                     )
                 })}
             </section>
+            <section className='slide-form'>
+                <div className='container'>
+                    <h2>Enjoy Your Holiday</h2>
+                    <span>Search and Book Hotel</span>
+
+                    <form action=''>
+                        <input type='text' placeholder='Search City' name='' id=''/>
+                        <div className='flex_space'>
+                            <input type='date' placeholder='Check In'/>
+                            <input type='date' placeholder='Check Out'/>
+                        </div>
+                        <div className='flex_space'>
+                            <input type='number' placeholder='Adult(s)(+18)'/>
+                            <input type='number' placeholder='Adult(s)(0-17)'/>
+                        </div>
+                        <input type='number' placeholder='Rooms'/>
+                        <input type='Submit' value='Search' className='submit' placeholder='Rooms'/>
+                    </form>
+                </div>
+            </section>
         </>
     )
 }
